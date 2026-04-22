a9db5fca0d8a9f545b87cf25118e62ae5fff45fb
matiuho <matiasvconchaf@gmail.com>
2026-04-21 20:57:43 -0400

    ordenando carpetas
    ordenando para los commit

---
+++ G-main/Proyecto/Proyecto/Stack/Microservicios/medical-service/medical-service/src/main/java/cl/duoc/rednorte/medical_service/MedicalServiceApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.rednorte.medical_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalServiceApplication.class, args);
+	}
+
+}
---
+++ G-main/Proyecto/Proyecto/Stack/Microservicios/medical-service/medical-service/src/test/java/cl/duoc/rednorte/medical_service/MedicalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.rednorte.medical_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
