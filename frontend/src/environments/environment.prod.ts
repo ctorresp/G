193ef22f857fcc730e4ff1e81a8d5ffb2cb92da8
matiuho <matiasvconchaf@gmail.com>
2026-07-02 01:05:23 -0400

    Revert "a"
    This reverts commit 6a2db5c67c5a5fe176b4d3ef5f0ba1f8b6cebf06.

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  production: true, // false en el archivo local
-  supabaseUrl: 'https://byqsdgspbsowdmysyaal.supabase.co/rest/v1/',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5cXNkZ3NwYnNvd2RteXN5YWFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5MzIyNTMsImV4cCI6MjA5ODUwODI1M30.JVUL0rZSMAOX_tydKmxnNTMSArvuwJEC1ZUskAjpeZ8'
+  production: true,
+  apiUrl: '/api'
 };
